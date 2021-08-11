daca77fecb739437ad1ad17371ab83d5a156cda6
Clodoaldo Favaro <clodoaldofavero@gmail.com>
2021-08-10 23:33:25 -0300

    Monta o labirinto aleatoriamente.

--- app/src/main/java/com/example/labirinto/GameView.java
+++ app/src/main/java/com/example/labirinto/GameView.java
@@ -9,12 +9,17 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
+
 public class GameView extends View {
     private Cell[][] cells;
     private static final int COLS = 7, ROWS = 10;
     private static final float WALL_THICKNESS = 4;
     private float cellSize, hMargin, vMargin;
     private Paint wallPaint;
+    private Random random;
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -23,16 +28,94 @@ public class GameView extends View {
         wallPaint.setColor(Color.BLACK);
         wallPaint.setStrokeWidth(WALL_THICKNESS);
 
+        random = new Random();
+
         createMaze();
     }
 
     private void createMaze() {
+        Stack<Cell> stack = new Stack<>();
+        Cell current, next;
+
         cells = new Cell[COLS][ROWS];
 
         for (int x = 0; x < COLS; x++) {
             for (int y = 0; y < ROWS; y++) {
                 cells[x][y] = new Cell(x, y);
             }
+        }
+
+        current = cells[0][0];
+        current.visited = true;
+
+        do {
+            next = getNeighbour(current);
+            if (next != null) {
+                removeWall(current, next);
+                stack.push(current);
+                current = next;
+                current.visited = true;
+            } else {
+                current = stack.pop();
+            }
+        } while (!stack.empty());
+
+    }
+
+    private Cell getNeighbour(Cell cell) {
+        ArrayList<Cell> neighbours = new ArrayList<>();
+
+        //left neighbour
+        if (cell.col > 0 && !cells[cell.col - 1][cell.row].visited) {
+            neighbours.add(cells[cell.col - 1][cell.row]);
+        }
+
+        //right neighbour
+        if (cell.col < (COLS - 1)  && !cells[cell.col + 1][cell.row].visited) {
+            neighbours.add(cells[cell.col + 1][cell.row]);
+        }
+
+        //top neighbour
+        if (cell.row > 0 && !cells[cell.col][cell.row - 1].visited) {
+            neighbours.add(cells[cell.col][cell.row - 1]);
+        }
+
+        //bottom neighbour
+        if (cell.row < (ROWS - 1) && !cells[cell.col][cell.row + 1].visited) {
+            neighbours.add(cells[cell.col][cell.row + 1]);
+        }
+
+        if (neighbours.size() > 0 ) {
+            int index = random.nextInt(neighbours.size());
+            return neighbours.get(index);
+        }
+
+        return null;
+    }
+
+    private void removeWall(Cell current, Cell next) {
+        //current under next
+        if (current.col == next.col && current.row == next.row + 1) {
+            current.topWall = false;
+            next.bottomWall = false;
+        }
+
+        //current above next
+        if (current.col == next.col && current.row == next.row - 1) {
+            current.bottomWall = false;
+            next.topWall = false;
+        }
+
+        //current to the right of the next
+        if (current.col == next.col + 1 && current.row == next.row) {
+            current.leftWall = false;
+            next.rightWall = false;
+        }
+
+        //current to the left of the next
+        if (current.col == next.col - 1 && current.row == next.row) {
+            current.rightWall = false;
+            next.leftWall = false;
         }
     }
 
@@ -60,11 +143,11 @@ public class GameView extends View {
                     canvas.drawLine(x*cellSize, y*cellSize, (x + 1)*cellSize, y*cellSize, wallPaint);
                 }
 
-                if (cells[x][y].leftWall) {
+                if (cells[x][y].leftWall && !(x == 0 && y == 0)) {
                     canvas.drawLine(x*cellSize, y*cellSize, x*cellSize, (y+1)*cellSize, wallPaint);
                 }
 
-                if (cells[x][y].rightWall) {
+                if (cells[x][y].rightWall && !(x == COLS - 1 && y == ROWS - 1)) {
                     canvas.drawLine((x+1)*cellSize, y*cellSize, (x + 1)*cellSize, (y+1)*cellSize, wallPaint);
                 }
 
@@ -76,7 +159,7 @@ public class GameView extends View {
     }
 
     private class Cell {
-        boolean topWall = true, leftWall = true, bottomWall = true, rightWall = true;
+        boolean topWall = true, leftWall = true, bottomWall = true, rightWall = true, visited = false;
 
         int col , row;
 
