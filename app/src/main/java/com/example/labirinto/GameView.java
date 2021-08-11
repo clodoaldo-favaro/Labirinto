67a3d4fe76dc0077d4fb7de42fcc08908c89902b
Clodoaldo Favaro <clodoaldofavero@gmail.com>
2021-08-11 01:05:52 -0300

    Implementa movimento; gera novo labirinto ao atingir a saida.

--- app/src/main/java/com/example/labirinto/GameView.java
+++ app/src/main/java/com/example/labirinto/GameView.java
@@ -1,10 +1,13 @@
 package com.example.labirinto;
 
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Vibrator;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -14,11 +17,20 @@ import java.util.Random;
 import java.util.Stack;
 
 public class GameView extends View {
+
+    private enum Direction {
+        UP, DOWN, LEFT, RIGHT
+    }
+
+    private Vibrator vibrator;
+    private Context mContext;
+
     private Cell[][] cells;
+    private Cell player, exit;
     private static final int COLS = 7, ROWS = 10;
     private static final float WALL_THICKNESS = 4;
     private float cellSize, hMargin, vMargin;
-    private Paint wallPaint;
+    private Paint wallPaint, playerPaint, exitPaint;
     private Random random;
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
@@ -28,7 +40,15 @@ public class GameView extends View {
         wallPaint.setColor(Color.BLACK);
         wallPaint.setStrokeWidth(WALL_THICKNESS);
 
+        playerPaint = new Paint();
+        playerPaint.setColor(Color.RED);
+
+        exitPaint = new Paint();
+        exitPaint.setColor(Color.BLUE);
+
         random = new Random();
+
+        vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 
         createMaze();
     }
@@ -44,6 +64,9 @@ public class GameView extends View {
                 cells[x][y] = new Cell(x, y);
             }
         }
+
+        player = cells[0][0];
+        exit = cells[COLS - 1][ROWS - 1];
 
         current = cells[0][0];
         current.visited = true;
@@ -156,6 +179,114 @@ public class GameView extends View {
                 }
             }
         }
+
+        float margin = cellSize/10;
+
+        canvas.drawRect(
+                player.col*cellSize+margin,
+                player.row*cellSize+margin,
+                (player.col + 1)*cellSize-margin,
+                (player.row + 1)*cellSize-margin,
+                playerPaint
+        );
+
+        /*
+        canvas.drawRect(
+                exit.col*cellSize+margin,
+                exit.row*cellSize+margin,
+                (exit.col + 1)*cellSize-margin,
+                (exit.row + 1)*cellSize-margin,
+                exitPaint
+        );
+        */
+    }
+
+    private void movePlayer(Direction direction) {
+        switch (direction) {
+            case UP:
+                if (!player.topWall) {
+                    player = cells[player.col][player.row - 1];
+                } else {
+                    vibrator.vibrate(400);
+                }
+                break;
+            case DOWN:
+                if (!player.bottomWall) {
+                    player = cells[player.col][player.row + 1];
+                } else {
+                    vibrator.vibrate(400);
+                }
+                break;
+            case LEFT:
+                if (!(player.leftWall || player.col == 0)) {
+                    player = cells[player.col - 1][player.row];
+                } else {
+                    vibrator.vibrate(400);
+                }
+                break;
+            case RIGHT:
+                if (!player.rightWall) {
+                    player = cells[player.col + 1][player.row];
+                } else {
+                    vibrator.vibrate(400);
+                }
+                break;
+        }
+
+        checkExit();
+        invalidate();
+    }
+
+    private void checkExit() {
+        if (player == exit) {
+            createMaze();
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            return true;
+        }
+
+        if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            float x = event.getX();
+            float y = event.getY();
+
+            float playerCenterX = hMargin + (player.col + 0.5f)*cellSize;
+            float playerCenterY = vMargin + (player.row + 0.5f)*cellSize;
+
+            float dx = x - playerCenterX;
+            float dy = y - playerCenterY;
+
+            float absDx = Math.abs(dx);
+            float absDy = Math.abs(dy);
+
+            if (absDx > cellSize || absDy > cellSize) {
+                if (absDx > absDy) {
+                    //move in x-direction
+                    if (dx > 0) {
+                        //move to the right
+                        movePlayer(Direction.RIGHT);
+                    } else {
+                        //move to the left
+                        movePlayer(Direction.LEFT);
+                    }
+                } else {
+                    //move in y-direction
+                    if (dy > 0) {
+                        //move down
+                        movePlayer(Direction.DOWN);
+                    } else {
+                        //move up
+                        movePlayer(Direction.UP);
+                    }
+                }
+            }
+            return true;
+        }
+
+        return super.onTouchEvent(event);
     }
 
     private class Cell {
