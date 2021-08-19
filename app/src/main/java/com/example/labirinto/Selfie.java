e186fefb9edb17b9046e323ebd04ab83c8a38ed7
Clodoaldo Favaro <clodoaldofavero@gmail.com>
2021-08-19 19:52:11 -0300

    Ajusta layout, fontes de botoes, e comeca a fazer o comportamento da camera.

--- app/src/main/java/com/example/labirinto/Selfie.java
+++ app/src/main/java/com/example/labirinto/Selfie.java
@@ -3,18 +3,36 @@ package com.example.labirinto;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Selfie extends AppCompatActivity {
 
+    ImageView imageViewSelfie;
+    Button takePictureBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.selfie);
+
+        imageViewSelfie = findViewById(R.id.imageViewSelfie);
+        takePictureBtn = findViewById(R.id.buttonTakeSelfie);
+
+        takePictureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     public void game(View v) {
         startActivity(new Intent(this, com.example.labirinto.Game.class));
     }
+
+
+
 }
